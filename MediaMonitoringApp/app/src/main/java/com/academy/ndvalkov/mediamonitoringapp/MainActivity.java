7f1b4f2a7c2d335cc3a1b0a566ab8a1aae7480e7
nicke <ndvalkov@abv.bg>
2017-09-21 10:24:14 +0300

    Blank starter

---
+++ MediaMonitoringApp/app/src/main/java/com/academy/ndvalkov/mediamonitoringapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.academy.ndvalkov.mediamonitoringapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
